986023fc4c5702805db3d27f2ce0500ac33d6221
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2024-09-17 21:02:27 +0200

    3.10 version in matrix read as 3.1?

--- tests/test_resolve.py
+++
@@ -1,8 +0,0 @@
-from bibmancli import resolve
-
-
-def test_resolve_identifier():
-    identifier = "10.1002/andp.19053221004"
-    response = resolve.resolve_identifier(identifier, 5.0)
-    assert response["author"] == "Einstein, A."
-    assert response["ENTRYTYPE"] == "article"
